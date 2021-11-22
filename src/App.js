811b27c1450154f46571f1289bf5fa3495aebd49
mfflynnnn <yougotanemailfrommolly@gmail.com>
2021-11-22 15:27:30 -0800

    Created form in Dictionary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
+import "./Dictionary.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello world!</header>
+      <div className="container">
+        <header className="App-header">Hello world!</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This dictionary app was coded by Molly Flynn. High five!
+        </footer>
+      </div>
     </div>
   );
 }
